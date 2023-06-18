274c110acca5975502b1df8fa4b4c7c3952364b3
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2023-06-18 20:32:49 +0800

    refactor route changer to route manager class

--- Flutter Projects (stand-alone)/spotify_clone/lib/main.dart
+++ Flutter Projects (stand-alone)/spotify_clone/lib/main.dart
@@ -3,15 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:spotify_clone/providers/global_state_provider.dart';
-import 'package:spotify_clone/screens/login_screen.dart';
-import 'package:spotify_clone/screens/sign_up/enter_email_screen.dart';
-import 'package:spotify_clone/screens/sign_up/enter_gender_screen.dart';
-import 'package:spotify_clone/screens/sign_up/enter_name_screen.dart';
-import 'package:spotify_clone/screens/sign_up/enter_password_screen.dart';
-import 'package:spotify_clone/screens/sign_up/select_artists_screen.dart';
+import 'package:spotify_clone/utilities/route_manager.dart';
 import 'package:spotify_clone/screens/welcome_screen.dart';
-import 'package:spotify_clone/utilities/create_route_to.dart';
-
 import 'firebase_options.dart';
 
 /// TIPS TO REMEMBER:
@@ -20,7 +13,7 @@ import 'firebase_options.dart';
 /// 3. Keep track of all the routes so that you don't lose track of where you are
 /// 4. Refactor all the code to make it more readable
 /// 5. Have a global app_theme file to store all the theme data (colors, fonts, etc.)
-/// 6.
+/// 6. ??
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -63,31 +56,7 @@ class SpotifyCloneApp extends StatelessWidget {
           return MaterialApp(
             key: SpotifyCloneApp.mainAppKey,
             scaffoldMessengerKey: GlobalKey<ScaffoldMessengerState>(),
-            onGenerateRoute: (settings) {
-              if (settings.name == "/welcome_screen") {
-                return createRouteTo(const WelcomeScreen(), settings);
-              }
-              if (settings.name == "/sign_up_email_screen") {
-                return createRouteTo(const EnterEmailScreen(), settings);
-              }
-              if (settings.name == "/sign_up_password_screen") {
-                return createRouteTo(const EnterPasswordScreen(), settings);
-              }
-              if (settings.name == "/sign_up_gender_screen") {
-                return createRouteTo(const EnterGenderScreen(), settings);
-              }
-              if (settings.name == "/sign_up_name-perm_screen") {
-                return createRouteTo(const EnterNameScreen(), settings);
-              }
-              if (settings.name == "/sign_up_select-artist_screen") {
-                return createRouteTo(const SelectArtistScreen(), settings);
-              }
-              if (settings.name == "/login_screen") {
-                return createRouteTo(const LoginScreen(), settings);
-              }
-              // return MaterialPageRoute(builder: (context) => const WelcomeScreen());
-              return null;
-            },
+            onGenerateRoute: (settings) => SpotifyRouteManager.goToScreen(settings),
             color: const Color.fromARGB(166, 27, 26, 28),
             debugShowCheckedModeBanner: false,
             home:
--- Flutter Projects (stand-alone)/spotify_clone/lib/utilities/route_manager.dart
+++ Flutter Projects (stand-alone)/spotify_clone/lib/utilities/route_manager.dart
@@ -1,10 +1,38 @@
+import 'package:flutter/material.dart';
 
+import 'create_route_to.dart';
+import 'package:spotify_clone/screens/login_screen.dart';
+import 'package:spotify_clone/screens/sign_up/enter_email_screen.dart';
+import 'package:spotify_clone/screens/sign_up/enter_gender_screen.dart';
+import 'package:spotify_clone/screens/sign_up/enter_name_screen.dart';
+import 'package:spotify_clone/screens/sign_up/enter_password_screen.dart';
+import 'package:spotify_clone/screens/sign_up/select_artists_screen.dart';
+import 'package:spotify_clone/screens/welcome_screen.dart';
 
-// create a separate class to handle all the route generation tasks that was 
-// previously in the main.dart file
-
-
-
-
-
-
+class SpotifyRouteManager {
+  static Route<dynamic>? goToScreen(RouteSettings settings) {
+    if (settings.name == WelcomeScreen.routeName) {
+      return createRouteTo(const WelcomeScreen(), settings);
+    }
+    if (settings.name == EnterEmailScreen.routeName) {
+      return createRouteTo(const EnterEmailScreen(), settings);
+    }
+    if (settings.name == EnterPasswordScreen.routeName) {
+      return createRouteTo(const EnterPasswordScreen(), settings);
+    }
+    if (settings.name == EnterGenderScreen.routeName) {
+      return createRouteTo(const EnterGenderScreen(), settings);
+    }
+    if (settings.name == EnterNameScreen.routeName) {
+      return createRouteTo(const EnterNameScreen(), settings);
+    }
+    if (settings.name == SelectArtistScreen.routeName) {
+      return createRouteTo(const SelectArtistScreen(), settings);
+    }
+    if (settings.name == LoginScreen.routeName) {
+      return createRouteTo(const LoginScreen(), settings);
+    }
+    // return MaterialPageRoute(builder: (context) => const WelcomeScreen());
+    return null;
+  }
+}
